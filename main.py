3f5076742ed84968a55965ca89a68254f1a0738e
SeaMoose6 <tophtoph05@gmail.com>
2021-09-23 09:40:14 -0400

    DRAWING

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
+import pygame
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+
+#constants
+SKY_BLUE = (53, 171, 184)
+SAND = (201, 146, 50)
+UMBRELLA = (201, 48, 34)
+WHITE = (255, 255, 255)
+OCEAN = (32, 98, 179)
+BLACK = (0, 0, 0)
+SUN = (255, 187, 0)
+WAVES = (4, 52, 110)
+
+PI = math.pi
+
+SIZE = (1000, 750)
+FPS = 60
+
+pygame.init()
+
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("The Beach")
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+
+        # check for specific user event
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(SKY_BLUE)
+
+    # beach
+    pygame.draw.rect(screen, OCEAN, [0, 300, 1000, 1000])
+    pygame.draw.rect(screen, SAND, [0, 600, 1000, 1000])
+    pygame.draw.arc(screen, SAND, [-50, 500, 300, 700], 0, 180, 250)
+    pygame.draw.arc(screen, SAND, [0, 500, 500, 700], 0, 180, 250)
+    pygame.draw.arc(screen, SAND, [100, 500, 600, 700], 0, 180, 250)
+    pygame.draw.arc(screen, SAND, [200, 550, 700, 700], 0, 180, 250)
+    pygame.draw.arc(screen, SAND, [700, 500, 500, 700], 0, 180, 2250)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # umbrella
+    pygame.draw.line(screen, BLACK, (650, 600), (700, 150), 10)
+    pygame.draw.arc(screen, UMBRELLA, [400, 50, 600, 350], 0, PI, 10)
+    pygame.draw.line(screen, UMBRELLA, (401, 215), (1000, 220), 10)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # sun
+    pygame.draw.circle(screen, SUN, (0, 0), 150)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # wave
+    def wave(x, y):
+        pygame.draw.arc(screen, WAVES, [x, y, x+50, y+75], 0, PI/2, 10)
+        pygame.draw.arc(screen, WAVES, [x, y, x + 20, y + 75], 0, PI / 2.5, 10)
+        #pygame.draw.polygon(screen, OCEAN, [(x+75, y+100), (x+130, y+75), (x+200, y+100)])
+
+    wave(100, 100)
+
+    pygame.display.flip()
+
+    clock.tick(FPS)
+
+pygame.quit()
